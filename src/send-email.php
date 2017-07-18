e37bdcc4289b3114b115626b2a0d68a48f8929af
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2017-07-18 12:44:50 +0300

    still testing email

--- src/send-email.php
+++ src/send-email.php
@@ -2,7 +2,17 @@
 
 require '../vendor/autoload.php';
 
-$msgClient = new \Mailgun\Mailgun( 'key-81ca3afdb5b337bfc40d2ee4a1393521' );
+$client = new \GuzzleHttp\Client;
+$client->post("https://api:key-81ca3afdb5b337bfc40d2ee4a1393521@api.mailgun.net/v3/sandboxa95bf897f1554f39b8796949f40f36c1.mailgun.org/messages", array(
+	'form_params' => array(
+		'from' => 'Excited User<test@betesoftware.com>',
+		'to' => 'briktyfissehaye@gmail.com',
+		'subject' => 'Hello',
+		'text' => 'Testing from PHP code'
+	)
+));
+
+/*$msgClient = new \Mailgun\Mailgun( 'key-81ca3afdb5b337bfc40d2ee4a1393521' );
 $domain    = "https://api.mailgun.net/v3/sandboxa95bf897f1554f39b8796949f40f36c1.mailgun.org";
 
 $result = $msgClient->sendMessage( "$domain",
@@ -14,4 +24,4 @@ $result = $msgClient->sendMessage( "$domain",
 	) );
 
 header('Content-Type', 'application/json');
-json_encode(['status' => 'succes', 'result' => $result]);
+json_encode(['status' => 'succes', 'result' => $result]);*/

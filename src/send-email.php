c8d476dafbc9ca6ef9e9ddd198715e4bb98c2f4e
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2017-07-18 12:53:43 +0300

    still testing email

--- src/send-email.php
+++ src/send-email.php
@@ -2,15 +2,27 @@
 
 require '../vendor/autoload.php';
 
+if(isset($_POST['email'])) {
+	header('Content-Type: application/json');
+	echo json_encode(['status' => 'failure', 'error' => 'You must provide email address']);
+	exit();
+}
+
 $client = new \GuzzleHttp\Client;
-$client->post("https://api:key-81ca3afdb5b337bfc40d2ee4a1393521@api.mailgun.net/v3/sandboxa95bf897f1554f39b8796949f40f36c1.mailgun.org/messages", array(
+$response = $client->post("https://api:key-81ca3afdb5b337bfc40d2ee4a1393521@api.mailgun.net/v3/sandboxa95bf897f1554f39b8796949f40f36c1.mailgun.org/messages", array(
 	'form_params' => array(
-		'from' => 'Excited User<test@betesoftware.com>',
-		'to' => 'briktyfissehaye@gmail.com',
+		'from' => 'Tester Bete Software<test@betesoftware.com>',
+		'to' => $_POST['email'],
 		'subject' => 'Hello',
 		'text' => 'Testing from PHP code'
 	)
 ));
+header('Content-Type: application/json');
+if($response->getBody()) {
+	echo json_encode(['status' => 'success', 'message' => 'Email has been sent']);
+} else {
+	echo json_encode(['status' => 'failure', 'message' => 'Could not send email. Please try again.']);
+}
 
 /*$msgClient = new \Mailgun\Mailgun( 'key-81ca3afdb5b337bfc40d2ee4a1393521' );
 $domain    = "https://api.mailgun.net/v3/sandboxa95bf897f1554f39b8796949f40f36c1.mailgun.org";
